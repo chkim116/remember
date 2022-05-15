b5c348b945341bc4112faadebf9fe89c2cc3b942
chkim116 <dudnqnfqlc@naver.com>
2022-05-15 14:29:42 +0900

    feat: 초기 커밋
    - vite 기반 CRA
    - antd, react-router-dom 설치
    - styled-components 설치

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Wrap = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
+	min-height: 90vh;
+`;
+
+function App() {
+	return <Wrap>app</Wrap>;
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import App from './App';
+
+import 'antd/dist/antd.css';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<App />} />
+			</Routes>
+		</BrowserRouter>
+	</React.StrictMode>
+);
